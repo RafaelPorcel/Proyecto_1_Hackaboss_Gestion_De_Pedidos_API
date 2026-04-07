a2a07826cedd5998ba2e7b83c1c4bd204a5b24eb
RafaPorcel <rafaelporcelllobregat@gmail.com>
2026-04-07 19:58:16 +0200

    feat: crear clase Pedido

--- src/main/java/com/example/gestion_de_pedidos_api/model/Pedido.java
+++ src/main/java/com/example/gestion_de_pedidos_api/model/Pedido.java
@@ -1,4 +1,36 @@
 package com.example.gestion_de_pedidos_api.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "pedido")
+// Esta clase hace referencia al 'Ticket de compra' -> nuestro Pedido
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String codigo;
+    private LocalDateTime fecha;
+    private Double total;
+    @Enumerated(EnumType.STRING) // @Enumerated para decir a la bbdd que este campo es una lista cerrada de pciones, puesto que es un enum
+    // Y EnumType.STRING para decir a la bb que guarde el nombre del estado como texto, si no lo haría como un número
+    private EstadoPedido estadoPedido;
+    @ManyToOne// Relación muchos a uno, muchos pedidos pertenecen a una determinada terminal
+    @JoinColumn(name = "terminal_id", nullable = false)//Para relacionarlo con el id de Terminal. Esta es la foreing key que se relaciona con la primary key de Terminal
+    private Terminal terminal;
+    // Un pedido puede tener muchos artículos (líneas de pedido).
+    // mappedBy indica que la relación se controla desde el campo 'pedido' de la clase PedidoProducto.
+    // CascadeType.ALL es para la base de datos, que si guardas/borras el Pedido, sus líneas se guardan/borran solas.
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<PedidoProducto> lineasPedido; // Esta lista es el "carrito" o desglose de productos del ticket
 }
