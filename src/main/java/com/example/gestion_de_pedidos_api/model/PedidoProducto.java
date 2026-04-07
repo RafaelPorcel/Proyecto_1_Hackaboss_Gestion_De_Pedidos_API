09840f3903fd6fc28da6731f4609a2eb4cf07371
Dani Norbert <daninor01@gmail.com>
2026-04-07 19:24:07 +0200

    feat: creacion de clase PedidoProducto

--- src/main/java/com/example/gestion_de_pedidos_api/model/PedidoProducto.java
+++ src/main/java/com/example/gestion_de_pedidos_api/model/PedidoProducto.java
@@ -1,4 +1,26 @@
 package com.example.gestion_de_pedidos_api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pedido_producto")
 public class PedidoProducto {
+    @Id
+    private Long id;
+    private Integer cantidad;
+    private Double precioUnitario;
+    @ManyToOne
+    private Pedido pedido;
+    @ManyToOne
+    private Producto producto;
+
 }
