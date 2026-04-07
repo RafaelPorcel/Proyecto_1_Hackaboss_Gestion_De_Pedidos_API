9edb5fbda76b4a0db51d83edf9ce48073a95531d
RafaPorcel <rafaelporcelllobregat@gmail.com>
2026-04-07 12:49:04 +0200

    feat: acabar Terminal

--- src/main/java/com/example/gestion_de_pedidos_api/model/Terminal.java
+++ src/main/java/com/example/gestion_de_pedidos_api/model/Terminal.java
@@ -1,4 +1,19 @@
 package com.example.gestion_de_pedidos_api.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "terminal")
 public class Terminal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String nombre;
+    @OneToMany (mappedBy = "terminal")
+    private List<Pedido> listaPedidos;
+
 }
