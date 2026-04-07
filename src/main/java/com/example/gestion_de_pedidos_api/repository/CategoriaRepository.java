21633fcd3c736328f8f8b8f568620cccefc50b96
Laura <ariasleonlaura@gmail.com>
2026-04-07 17:29:41 +0200

    fix:corregir CategoriaRepository, añadir Optional

--- src/main/java/com/example/gestion_de_pedidos_api/repository/CategoriaRepository.java
+++ src/main/java/com/example/gestion_de_pedidos_api/repository/CategoriaRepository.java
@@ -4,5 +4,8 @@ import com.example.gestion_de_pedidos_api.model.Categoria;
 import com.example.gestion_de_pedidos_api.model.Terminal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Optional<Categoria> findByNombre(String nombre);
 }
