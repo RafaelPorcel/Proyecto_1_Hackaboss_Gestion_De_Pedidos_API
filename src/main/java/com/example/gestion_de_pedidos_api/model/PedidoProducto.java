923b56bdace61c1cd12ef261b410cf9af007f89a
Dani Norbert <daninor01@gmail.com>
2026-04-07 19:41:32 +0200

    fix: cambios en la clase PedidoProducto

--- src/main/java/com/example/gestion_de_pedidos_api/model/PedidoProducto.java
+++ src/main/java/com/example/gestion_de_pedidos_api/model/PedidoProducto.java
@@ -1,26 +1,27 @@
 package com.example.gestion_de_pedidos_api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "pedido_producto")
+@Table(name = "pedidos_productos")
 public class PedidoProducto {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private Integer cantidad;
+    @Column(nullable = false)
     private Double precioUnitario;
     @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)
     private Pedido pedido;
     @ManyToOne
+    @JoinColumn(name = "producto_id", nullable = false)
     private Producto producto;
 
 }
