866ac1a31f48c75f168e2d308d9bc8935ae10e79
RafaPorcel <rafaelporcelllobregat@gmail.com>
2026-04-07 10:44:10 +0200

    feat: Crear models y sus clases

---
+++ src/main/java/com/example/gestion_de_pedidos_api/GestionDePedidosApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_de_pedidos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDePedidosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDePedidosApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestion_de_pedidos_api/model/Pedido.java
@@ -0,0 +1,4 @@
+package com.example.gestion_de_pedidos_api.model;
+
+public class Pedido {
+}
