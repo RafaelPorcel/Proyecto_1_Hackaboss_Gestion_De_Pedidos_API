ac30974123a40da99d1b7c0d4ac0afb470b53a29
Laura <ariasleonlaura@gmail.com>
2026-04-07 16:01:14 +0200

    feat:crear categoria y producto

--- src/main/java/com/example/gestion_de_pedidos_api/model/Producto.java
+++ src/main/java/com/example/gestion_de_pedidos_api/model/Producto.java
@@ -1,4 +1,31 @@
 package com.example.gestion_de_pedidos_api.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "productos")
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "precio", nullable = false)
+    private double precio;
+
+    @Column(name = "activo", nullable = false)
+    private boolean activo = true;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
 }
