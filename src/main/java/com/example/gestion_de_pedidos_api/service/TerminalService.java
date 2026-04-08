7227b79b2f4198093d0f5f31c022c9cdd3fac26f
RafaPorcel <rafaelporcelllobregat@gmail.com>
2026-04-08 16:54:02 +0200

    feat: añadir manejo de errores en metodo buscarTerminalPorId

--- src/main/java/com/example/gestion_de_pedidos_api/service/TerminalService.java
+++ src/main/java/com/example/gestion_de_pedidos_api/service/TerminalService.java
@@ -1,5 +1,6 @@
 package com.example.gestion_de_pedidos_api.service;
 
+import com.example.gestion_de_pedidos_api.exception.ResourceNotFoundException;
 import com.example.gestion_de_pedidos_api.model.Terminal;
 import com.example.gestion_de_pedidos_api.repository.TerminalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ public class TerminalService {
     }
 
     //Método para buscar una terminal mediante su Id
-    public Optional<Terminal> buscarTerminalPorId(Long id) {
-        return terminalRepository.findById(id);
+    public Terminal buscarTerminalPorId(Long id) {
+        return terminalRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("La terminal con id: " + id + " no existe"));
     }
 
     //Método para guardar una nueva terminal
