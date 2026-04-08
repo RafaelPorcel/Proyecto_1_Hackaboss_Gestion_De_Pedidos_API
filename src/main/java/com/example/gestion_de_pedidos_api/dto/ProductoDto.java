7e5e4265ab3c645203321320c31dfdb602e82820
Laura <ariasleonlaura@gmail.com>
2026-04-08 09:44:25 +0200

    feat:crear dto

--- src/main/java/com/example/gestion_de_pedidos_api/dto/ProductoDto.java
+++ src/main/java/com/example/gestion_de_pedidos_api/dto/ProductoDto.java
@@ -1,4 +1,18 @@
 package com.example.gestion_de_pedidos_api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductoDto {
+    private Long id;
+    private String nombre;
+    private Double precio;
+    private Boolean activo;
+    private String nombreCategoria;
 }
