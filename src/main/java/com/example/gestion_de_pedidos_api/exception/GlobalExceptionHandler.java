36b8fb6bb3cdaf27d81ac5999a6e62d28deac411
Dani Norbert <daninor01@gmail.com>
2026-04-08 18:14:09 +0200

    fix: correccion de nombres de metodos y excepcion requerida

--- src/main/java/com/example/gestion_de_pedidos_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/gestion_de_pedidos_api/exception/GlobalExceptionHandler.java
@@ -11,22 +11,22 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String,String>> manejarRecursoNoEncontrado(ResourceNotFoundException ex){
+    public ResponseEntity<Map<String,String>> handleResourceNotFound(ResourceNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Error: ", ex.getMessage()));
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<Map<String,String>> manejarSinStock(ResourceNotFoundException ex){
+    public ResponseEntity<Map<String,String>> handleOutOfStock(BadRequestException ex){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("Error: ", ex.getMessage()));
     }
 
     @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<Map<String,String>> manejarEstadoIlegal(IllegalStateException ex){
+    public ResponseEntity<Map<String,String>> handleIllegalState(IllegalStateException ex){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("Error: ", ex.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String,String>> manejarExcepcionesGenerales(Exception ex){
+    public ResponseEntity<Map<String,String>> handleGlobalExceptions(Exception ex){
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("Error inesperado: ", ex.getMessage()));
     }
 }
