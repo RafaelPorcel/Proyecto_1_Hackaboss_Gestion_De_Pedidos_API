8669e408a8d124e1354fa482a074413e20943e86
Laura <ariasleonlaura@gmail.com>
2026-04-09 23:27:02 +0200

    refactor: ajuste de nombres de tablas y columnas

--- src/main/java/com/example/gestion_de_pedidos_api/model/Pedido.java
+++ src/main/java/com/example/gestion_de_pedidos_api/model/Pedido.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name = "pedido")
+@Table(name = "pedidos")
 // Esta clase hace referencia al 'Ticket de compra' -> nuestro Pedido
 public class Pedido {
     @Id
