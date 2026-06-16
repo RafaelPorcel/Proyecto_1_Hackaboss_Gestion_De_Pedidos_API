eff15f62ef7ba4ede3314687627e190851098a0e
Dani Norbert <daninor01@gmail.com>
2026-06-16 09:41:11 +0200

    correcciones generales proyecto backend

--- src/main/java/com/example/gestion_de_pedidos_api/repository/TerminalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gestion_de_pedidos_api.repository;
-
-import com.example.gestion_de_pedidos_api.model.Terminal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TerminalRepository extends JpaRepository<Terminal, Long> {
-}
